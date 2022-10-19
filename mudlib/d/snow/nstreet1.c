398fa8f373ab9c2348dfbd5d7e0dc9762ddccf70
Annihilator <taedlar@gmail.com>
2022-10-19 20:09:25 +0800

    updated

--- mudlib/d/snow/nstreet1.c
+++ mudlib/d/snow/nstreet1.c
@@ -1,26 +1,28 @@
-// Room: /d/snow/nstreet1.c
+// vim: syntax=lpc
 
 inherit ROOM;
 
-void create()
+private void
+create()
 {
-	set("short", "街道");
-	set("long", @LONG
+  set("short", "街道");
+  set("long", @LONG
 這裡是雪亭鎮北邊的主要街道﹐往南通往鎮上最熱鬧的廣場﹐往北
-則通往鎮北的山路﹐你的兩邊都是一些賣雜貨的店鋪﹐這一代除了市集
-的日子﹐並不常有太多人走動。
+則通往鎮北的山路﹐你的西邊都是一些賣雜貨的店鋪﹐這一帶除了市集
+的日子﹐並不常有太多人走動。你注意到東邊有一間不太顯眼的土地公
+廟。
 LONG
-	);
-	set("detail", ([ /* sizeof() == 1 */
-		"店鋪" : "各式各樣的店鋪都有﹐不過看起來不像是你會用得著的地方。
-",
-]));
-	set("exits", ([ /* sizeof() == 2 */
-		"south" : __DIR__"square_n",
-		"north" : __DIR__"nstreet2",
-]));
-	set("outdoors", "snow");
+  );
+  set("detail", ([ /* sizeof() == 1 */
+	"店鋪" : "各式各樣的店鋪都有﹐不過看起來不像是你會用得著的地方。",
+  ]));
+  set("exits", ([ /* sizeof() == 2 */
+	"south" : __DIR__"square_n",
+	"north" : __DIR__"nstreet2",
+	"east": __DIR__"small_temple",
+  ]));
+  set("outdoors", "snow");
 
-	setup();
-	replace_program(ROOM);
+  setup();
+  replace_program(ROOM);
 }
--- mudlib/include/room.h
+++ mudlib/include/room.h
@@ -1,15 +1,16 @@
-#ifndef __ROOM__
-#define __ROOM__
+// vim: syntax=lpc
+#ifndef ROOM_H
+#define ROOM_H
 
-#define DOOR_CLOSED		1
-#define DOOR_LOCKED		2
+#define DOOR_CLOSED	1
+#define DOOR_LOCKED	2
 #define DOOR_SMASHED	4
-#define DOOR_HIDDEN		8
+#define DOOR_HIDDEN	8
 
-#define BANK        "/std/room/bank.c"
-#define HOCKSHOP    "/std/room/hockshop.c"
-#define INN         "/std/room/inn.c"
-#define JOURNEY     "/std/room/journey.c"
-#define HOCKSHOP2	"/std/room/hockshop2.c"
+#define BANK		"/std/room/bank.c"
+#define HOCKSHOP	"/std/room/hockshop.c"
+#define INN		"/std/room/inn.c"
+#define JOURNEY		"/std/room/journey.c"
+#define TEMPLE		"/std/room/temple.c"
 
-#endif
+#endif // ! ROOM_H
