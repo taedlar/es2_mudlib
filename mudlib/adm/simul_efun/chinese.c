235df46b6a7e121b8d04361265e8810243295e01
taedlar <taedlar@gmail.com>
2026-02-04 02:06:04 +0800

    fix: break_chinese_string

--- mudlib/adm/simul_efun/chinese.c
+++ mudlib/adm/simul_efun/chinese.c
@@ -30,34 +30,51 @@ to_chinese(string str)
     return CHINESE_D->chinese(str);
 }
 
-string
-break_chinese_string(string str, int len, mixed indent)
-{
-    string indent_str;
-    int cntr;
+string break_chinese_string (string str, int len, mixed indent) {
+    string* wch;
+    string indent_str, out, line;
+    int cntr, w;
 
-    if( !stringp(str) ) return 0;
+    if (!stringp (str))
+        return 0;
 
-    if( intp(indent) ) indent_str = sprintf("%*s", indent, " ");
-    else if( stringp(indent) ) indent_str = indent;
-    else indent_str = "";
+    if (intp (indent))
+        indent_str = sprintf ("%*s", indent, " ");
+    else if (stringp (indent))
+        indent_str = indent;
+    else
+        indent_str = "";
 
     cntr = 0;
 
     // strip all newlines.
-    str = replace_string(str, "\n", "");
+    str = replace_string (str, "\n", "");
 
-    while( strlen(str) > cntr + len ) {
-        int br, i;
-        for(i=0; i<len;) {
-            if( str[cntr+i]==' ' )
-                br = cntr+i;
-            i++;
+    // [NEOLITH] break up the string into array of unicode characters.
+    wch = explode (str, "");
+    out = "";
+    line = indent_str;
+    w = strlen(indent_str);
+    foreach (string ch in wch) {
+        line += ch;
+        if (strlen(ch) > 1) {
+            w += 2; // double width for multi-byte Chinese chars
         }
-        cntr+=i;
-        str = str[0..br-1] + "\n" + indent_str + str[br..<1];
+        else if (ch == "\t") {
+            w += 8 - w % 8; // set width to next tab stop
+        }
+        else {
+            w += 1;
+            if (ch != " " && ch != "\t")
+                continue; // only break on spaces for single byte chars
+        }
+        if (w >= len) {
+            out += line + "\n";
+            line = indent_str;
+            w = strlen(indent_str);
+        }
     }
-    return str;
+    return out + line;
 }
 
 string
--- mudlib/cmds/usr/score.c
+++ mudlib/cmds/usr/score.c
@@ -23,10 +23,11 @@ string display_attr(int gift, int value);
 string status_color(int current, int max);
 string tribar_graph(int val, int eff, int max);
 
-private void create() { seteuid(getuid()); }
+private void create() {
+    seteuid(getuid());
+}
 
-int main(object me, string arg)
-{
+int main(object me, string arg) {
     object ob;
     mapping score;
     string line, tmp, lform;
@@ -34,30 +35,33 @@ int main(object me, string arg)
 
     if(!arg) ob = me;
     else {
-	if( !wizardp(me) ) return notify_fail("只有巫師能察看別人的狀態。\n");
-	ob = present(arg, environment(me));
-	if (!ob) ob = find_player(arg);
-	if (!ob) ob = find_living(arg);
-	if (!ob) return notify_fail("你要察看誰的狀態﹖\n");
+        if( !wizardp(me) ) return notify_fail("只有巫師能察看別人的狀態。\n");
+        ob = present(arg, environment(me));
+        if (!ob) ob = find_player(arg);
+        if (!ob) ob = find_living(arg);
+        if (!ob) return notify_fail("你要察看誰的狀態﹖\n");
 /*  將check wiz 移至前面 -dragoon
-	if (!wizardp(me) && !ob->accept_info(me, "score") )
-	    return notify_fail("只有巫師能察看別人的狀態。\n");
+        if (!wizardp(me) && !ob->accept_info(me, "score") )
+            return notify_fail("只有巫師能察看別人的狀態。\n");
 */
     }
 
-    line = sprintf( HIW "【 %s 】" NOR "%s\n\n", ob->rank(), ob->short(1) );
-    line += break_chinese_string(
-	sprintf(" %s是一%s%s歲的 %2d 級%s%s%s﹐出生於%s，目前的業力是 %d 點。",
-		ob==me ? gender_self(ob) : gender_pronoun(ob),
-		(tmp = ob->query("unit")) ? tmp : "個",
-		chinese_number(ob->query("age")),
-		ob->query_level(),
-		(tmp = ob->query("gender")) ? to_chinese(tmp) : "",
-		to_chinese(ob->query_race()),
-		ob->query("humanoid") ? ob->rank(0, 1) : "",
-		CHINESE_D->chinese_date(((int)ob->query("birthday") - 14*365*24*60) * 60),
-		ob->link() ? ob->link()->query("karma") : 0 ),
-        	68, " ") + "\n\n";
+    line = sprintf (HIW "【 %s 】" NOR "%s\n\n", ob->rank(), ob->short(1));
+    line += break_chinese_string (sprintf(
+            "%s是一%s%s歲的 %d 級%s%s%s﹐出生於%s，目前的業力是 %d 點。",
+                ob==me ? gender_self(ob) : gender_pronoun(ob),
+                (tmp = ob->query("unit")) ? tmp : "個",
+                chinese_number(ob->query("age")),
+                ob->query_level(),
+                (tmp = ob->query("gender")) ? to_chinese(tmp) : "",
+                to_chinese(ob->query_race()),
+                ob->query("humanoid") ? ob->rank(0, 1) : "",
+                CHINESE_D->chinese_date(((int)ob->query("birthday") - 14*365*24*60) * 60),
+                ob->link() ? ob->link()->query("karma") : 0
+        ),
+        68,
+        " "
+    ) + "\n\n";
 
     if( wizardp(me) || (int)me->query_level() > 1 ) {
         line = sprintf(
@@ -80,59 +84,55 @@ int main(object me, string arg)
 
     if( ob->query_stat_maximum("gin") )
     line = sprintf("%s 精   %s%4d/%4d  " NOR GRN "%s\n" NOR, line,
-	status_color(ob->query_stat("gin"), ob->query_stat_maximum("gin")),
-	ob->query_stat("gin"),    ob->query_stat_maximum("gin"),
-	tribar_graph(ob->query_stat("gin"),    ob->query_stat_effective("gin"), ob->query_stat_maximum("gin")) );
+        status_color(ob->query_stat("gin"), ob->query_stat_maximum("gin")),
+        ob->query_stat("gin"),    ob->query_stat_maximum("gin"),
+        tribar_graph(ob->query_stat("gin"),    ob->query_stat_effective("gin"), ob->query_stat_maximum("gin")) );
     if( ob->query_stat_maximum("kee") )
     line = sprintf("%s 氣   %s%4d/%4d  " HIR "%s\n" NOR, line,
-	status_color(ob->query_stat("kee"), ob->query_stat_maximum("kee")),
-	ob->query_stat("kee"), ob->query_stat_maximum("kee"),
-	tribar_graph(ob->query_stat("kee"),    ob->query_stat_effective("kee"), ob->query_stat_maximum("kee")) );
+        status_color(ob->query_stat("kee"), ob->query_stat_maximum("kee")),
+        ob->query_stat("kee"), ob->query_stat_maximum("kee"),
+        tribar_graph(ob->query_stat("kee"),    ob->query_stat_effective("kee"), ob->query_stat_maximum("kee")) );
     if( ob->query_stat_maximum("sen") )
     line = sprintf("%s 神   %s%4d/%4d  " HIB "%s\n" NOR, line,
-	status_color(ob->query_stat("sen"), ob->query_stat_maximum("sen")),
-	ob->query_stat("sen"), ob->query_stat_maximum("sen"),
-	tribar_graph(ob->query_stat("sen"),    ob->query_stat_effective("sen"), ob->query_stat_maximum("sen")) );
+        status_color(ob->query_stat("sen"), ob->query_stat_maximum("sen")),
+        ob->query_stat("sen"), ob->query_stat_maximum("sen"),
+        tribar_graph(ob->query_stat("sen"),    ob->query_stat_effective("sen"), ob->query_stat_maximum("sen")) );
 
     line = sprintf("%s\n 食物 %s%4d/%4d" NOR "\t\t飲水 %s%4d/%4d" NOR "\t\t疲勞 %s%4d/%4d\n" NOR, line,
-	status_color(ob->query_stat("food"), ob->query_stat_maximum("food")),
-	ob->query_stat("food"), ob->query_stat_maximum("food"),
-	status_color(ob->query_stat("water"), ob->query_stat_maximum("water")),
-	ob->query_stat("water"), ob->query_stat_maximum("water"),
-	status_color(ob->query_stat_maximum("fatigue") - ob->query_stat("fatigue"), ob->query_stat_maximum("fatigue")),
-	ob->query_stat("fatigue"), ob->query_stat_maximum("fatigue") );
+        status_color(ob->query_stat("food"), ob->query_stat_maximum("food")),
+        ob->query_stat("food"), ob->query_stat_maximum("food"),
+        status_color(ob->query_stat("water"), ob->query_stat_maximum("water")),
+        ob->query_stat("water"), ob->query_stat_maximum("water"),
+        status_color(ob->query_stat_maximum("fatigue") - ob->query_stat("fatigue"), ob->query_stat_maximum("fatigue")),
+        ob->query_stat("fatigue"), ob->query_stat_maximum("fatigue") );
 
     score = ob->query("score");
-    if( mapp(score) )
-    {
-	string c;
-	int xp;
+    if (mapp (score)) {
+        string c;
+        int xp;
 
-	line += "\n 人物經驗/評價﹕\n   ";
-	i = 0;
-	foreach(c, xp in score)
-	{
-	    int xp_t;
+        line += "\n 人物經驗/評價﹕\n   ";
+        i = 0;
+        foreach (c, xp in score) {
+            int xp_t;
 
-	    if( xp >= 0 && (xp_t = ob->query_target_score(c) - xp) > 0 ) 
-		line = sprintf("%s" HIM "%-8s %8d %-9s " NOR, line,
-			to_chinese("score of " + c), xp, "[" + xp_t + "]");
-	    else
-		line = sprintf("%s%-8s %8d %9s ", line,
-			to_chinese("score of " + c), xp, "");
-	    if( (i++)%SCORE_COLUMN == SCORE_COLUMN-1 ) line += "\n   ";
-	}
-	if( i%SCORE_COLUMN != SCORE_COLUMN-1 ) line += "\n";
-	line += "\n";
+            if (xp >= 0 && (xp_t = ob->query_target_score(c) - xp) > 0) 
+                line = sprintf("%s" HIM "%-8s %8d %-9s " NOR, line, to_chinese("score of " + c), xp, "[" + xp_t + "]");
+            else
+                line = sprintf("%s%-8s %8d %9s ", line, to_chinese("score of " + c), xp, "");
+            if ((i++)%SCORE_COLUMN == SCORE_COLUMN-1)
+                line += "\n   ";
+        }
+        if (i%SCORE_COLUMN != SCORE_COLUMN-1)
+            line += "\n";
+        line += "\n";
     }
 
-    write(line);
+    write (line);
     return 1;
 }
 
-string
-display_attr(int gift, int value)
-{
+string display_attr(int gift, int value) {
     if( value > gift )
         return sprintf( HIC "%3d%5s" NOR, value, "(+" + (value - gift) + ")" );
     else if( value < gift )
@@ -141,29 +141,25 @@ display_attr(int gift, int value)
         return sprintf( CYN "%3d     " NOR, value);
 }
 
-string
-status_color(int current, int max)
-{
+string status_color(int current, int max) {
     if( (max<=0) || (current > max) )
-	return HIC;		/* > 100%	青色 */
+        return HIC;		/* > 100%	青色 */
 
     switch(current*10/max) {
     case 10: case 9:		/* 100% - 90%	綠色 */
-	return HIG;
+        return HIG;
     case 8: case 7: case 6:	/* 90% - 60%	黃色 */
-	return HIY;
+        return HIY;
     case 5: case 4: case 3:	/* 60% - 30%	暗黃色 */
-	return YEL;
+        return YEL;
     case 2: case 1:		/* 30% - 10%	紅色 */
-	return HIR;
+        return HIR;
     default:			/* < 10%	暗紅色 */
-	return RED;
+        return RED;
     }
 }
 
-string
-tribar_graph(int val, int eff, int max)
-{
+string tribar_graph(int val, int eff, int max) {
     int n_filled, n_empty;
     string bar = "";
 
@@ -183,9 +179,7 @@ tribar_graph(int val, int eff, int max)
     return bar;
 }
 
-int
-help(object me)
-{
+int help(object me) {
         write(@HELP
 指令格式 : score
            score <對象名稱>                   (巫師專用)
