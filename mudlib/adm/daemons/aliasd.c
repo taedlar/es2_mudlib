ab850234b9625b02d0eaf0a9c99c257d74cfcc8a
Taedlar <taedlar@gmail.com>
2026-06-02 17:25:55 +0800

    feat: improve alias processing

--- mudlib/adm/daemons/aliasd.c
+++ mudlib/adm/daemons/aliasd.c
@@ -1,8 +1,9 @@
-
+/*---
+description: Process global aliases.
+author: Annihilator <taedlar@gmail.com>
+---*/
 
 #pragma save_binary
-
-//inherit F_CLEAN_UP;
 
 mapping global_alias = ([
     "l":	"look",
@@ -30,23 +31,30 @@ mapping global_alias = ([
     "k":	"kill"
 ]);
 
-void create()
-{
-    seteuid(getuid());
+void create() {
+    // Do not set EUID since the alias daemon never load objects and does not need to write files.
+    // seteuid(getuid());
 }
 
-string process_global_alias(string arg)
-{
-    string *word;
+/*
+ * Process global aliases.
+ * The alias daemon only process global aliases, and the user defined aliases are processed
+ * in the user object. This provides a newbie-friendly initial set of aliases and also allows
+ * veteran players to override the global aliases with their own ones.
+ *
+ * NOTE: This is called for EVERY commands from EVERY user, we only do simple string replacement
+ * here without argument parsing. The user defined aliases are processed in the user object,
+ * which allows more complex alias definitions with argument parsing.
+ */
+string process_global_alias (string cmd) {
+    string verb = query_verb();
 
-    if (arg[0]=='\'') return "say " + arg[1..];
+    if (cmd[0]=='\'')
+        return "say " + cmd[1..];
 
-    word = explode(arg, " ");
-    if( sizeof(word) && !undefinedp(global_alias[word[0]]) ) {
-        word[0] = global_alias[word[0]];
-        return implode(word, " ");
-    }
+    if (!undefinedp(global_alias[verb]))
+        return global_alias[verb] + cmd[verb.len()..];
 
-    return arg;
+    return cmd;
 }
 
