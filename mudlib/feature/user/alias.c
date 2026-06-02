372265df825ebbdde5151a27c7cc59859320ce52
Ted Chang <taedlar@gmail.com>
2026-06-02 17:41:06 +0800

    perf: improve alias processing (#30)
    * feat: improve alias processing
    
    * chore: use map iterator

--- mudlib/feature/user/alias.c
+++ mudlib/feature/user/alias.c
@@ -1,73 +1,62 @@
-
+/*---
+description: Process user defined aliases.
+author: Annihilator <taedlar@gmail.com>
+---*/
 
 #define MAX_ALIASES 40
 
 mapping alias;
 
-string
-process_alias(string str)
-{
-    if( mapp(alias) )
-    {
-	string cmd, *part;
-	int i, n_args;
+string process_alias (string str) {
+    if (mapp (alias)) {
+        string verb = query_verb();
+        if (!undefinedp (alias[str]))
+            return replace_string (alias[str], "$*", ""); // short cut for simple alias
 
-	if( !undefinedp(alias[str]) )
-	    return replace_string( alias[str], "$*", "" );
+        if (!undefinedp (alias[verb])) {
+            string* parts = explode (str, " ") - ({ "" });
+            int n_args = sizeof (parts);
 
-	i = strsrch(str, ' ');
-	if( i < 0 || undefinedp(alias[str[0..i-1]]) )
-	    return (string)ALIAS_D->process_global_alias(str);
+            string cmd = replace_string (alias[verb], "$*", str[verb.len() + 1 ..]);
+            for (int i = 1; i < n_args; i++)
+                cmd = replace_string (cmd, "$" + i, parts[i]);
 
-	part = explode(str, " ") - ({ "" });
-	n_args = sizeof(part);
-
-	cmd = replace_string( alias[part[0]], "$*", str[i+1..] );
-	for(i=1; i < n_args; i++)
-	    cmd = replace_string( cmd, "$" + i, part[i] );
-
-	return cmd;
+            return cmd;
+        }
     }
 
     return (string)ALIAS_D->process_global_alias(str);
 }
 
-int
-set_alias(string verb, string replace)
-{
-    if( geteuid(previous_object()) != ROOT_UID ) return 0;
+int set_alias (string verb, string replace) {
+    if (geteuid(previous_object()) != ROOT_UID)
+        return 0;
 
-    if( !replace ) {
-	if( mapp(alias) ) map_delete(alias, verb);
-	return 1;
+    if (!replace) {
+        if (mapp(alias))
+            map_delete (alias, verb);
+        return 1;
     }
 
-    if( !mapp(alias) ) {
-	alias = allocate_mapping(MAX_ALIASES);
-	alias[verb] = replace;
-	return 1;
-    }
+    if (!mapp(alias))
+        alias = allocate_mapping (MAX_ALIASES);
 
-    if( sizeof(alias) > MAX_ALIASES )
-	return notify_fail("您設定的 alias 太多了，請先刪掉一些不常用的。\n");
+    if (sizeof(alias) > MAX_ALIASES)
+        return notify_fail ("您設定的 alias 太多了，請先刪掉一些不常用的。\n");
 
     alias[verb] = replace;
     return 1;
 }
 
-mapping
-query_all_alias()
-{
-    if( geteuid(previous_object()) != ROOT_UID ) return 0;
-
+mapping query_all_alias() {
+    if (geteuid(previous_object()) != ROOT_UID)
+        return 0;
     return alias;
 }
 
-void
-set_all_alias(mapping a)
-{
-    if( geteuid(previous_object()) != ROOT_UID ) return;
-
+void set_all_alias(mapping a) {
+    if (geteuid(previous_object()) != ROOT_UID)
+        return;
     alias = a;
 }
 
