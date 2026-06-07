7c6423cd4e6d48d6389010123786cf38ce34a809
Ted Chang <taedlar@gmail.com>
2026-06-08 01:00:48 +0800

    area: wizard guild (#36)

--- mudlib/adm/guild/academy.c
+++ mudlib/adm/guild/academy.c
@@ -1,20 +1,23 @@
-// Room: /adm/guild/academy.c
-
+/*---
+descrition: 巫師學院
+author: Annihilator <taedlar@gmail.com>
+---*/
 inherit ROOM;
 
-void create()
-{
-	set("short", "巫師學院");
-	set("long", @LONG
-這裡是巫師們研究 LPC 的地方﹐如果你是新手巫師﹐歡迎來到這裡
-和其他人互相討論﹐如果你是老手﹐也請不吝回答新手的問題。
+void create() {
+    set ("short", "巫師學院");
+    set ("long", @LONG
+你來到一片平坦的草地，幾株大樹和樹下的大理石桌與舒適木椅提供了舒適的閱讀環境。
+草地外是看不見邊際的雲海，以及雲海上燦爛的星空。
+這裡是巫師們研究魔法與世界萬物的地方﹐如果你是新手巫師﹐歡迎來到這裡和其他人互相討論﹐如果你是老手﹐也請不吝回答新手的問題。
 LONG
-	);
-	set("exits", ([ /* sizeof() == 1 */
-		"west" : __DIR__"guildhall",
-	]));
+    );
+    set ("outdoors", "guild");
+    set ("exits", ([
+        "west" : __DIR__"guildhall",
+    ]));
 
-	setup();
-	load_object("/daemon/board/lpc");
-	replace_program(ROOM);
+    setup();
+    // load_object("/daemon/board/lpc");
+    replace_program (ROOM);
 }
--- mudlib/adm/guild/guildhall.c
+++ mudlib/adm/guild/guildhall.c
@@ -1,22 +1,25 @@
-// Room: /adm/guild/guildhall.c
-
+/*---
+descrition: 巫師公會
+author: Annihilator <taedlar@gmail.com>
+---*/
 inherit ROOM;
 
-void create()
-{
-	set("short", "巫師公會");
-	set("long", @LONG
-這裡就是巫師公會了﹐一個半球型的大浮島漂浮在七彩的雲間﹐平坦的地面鋪著一層碧綠草皮﹐
-草地盡頭是一棟純白的大型建築﹐希臘式的圓柱跟波斯風格的圓頂﹐營造出一種莊嚴的氣氛﹐
-往北是開會用的會議廳﹐東邊是研究 LPC 的巫師學院﹐往西則是通往世界各地的傳送站。
+void create() {
+    set ("short", "巫師公會");
+    set ("long", @LONG
+這裡就是巫師公會了﹐一個半球型的大浮島漂浮在七彩的雲間，平坦的地面鋪著一層碧綠草皮。
+草地北端盡頭是一棟純白的大型建築﹐希臘式的圓柱跟波斯風格的圓頂﹐營造出一種莊嚴的氣氛。
+你的東邊是露天的巫師學院﹐往西則是通往世界各地的傳送魔法陣。
 LONG
-	);
-	set("exits", ([ /* sizeof() == 2 */
-		"east" : __DIR__"academy",
-		"north" : __DIR__"meetingroom",
-]));
+    );
+    set ("outdoors", "guild");
+    set ("exits", ([
+        "east" : __DIR__"academy",
+        "west" : __DIR__"portal",
+        "north" : __DIR__"meetingroom",
+    ]));
 
-	setup();
-	load_object("/daemon/board/wizard");
-	replace_program(ROOM);
+    setup();
+    // load_object("/daemon/board/wizard");
+    replace_program (ROOM);
 }
