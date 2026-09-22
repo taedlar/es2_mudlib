d9d71d7584db1839c2346267b2d4780da7dfbce7
Taedlar <taedlar@gmail.com>
2026-09-22 16:19:20 +0800

    feat: add shutdown confirmation

--- mudlib/cmds/adm/shutdown.c
+++ mudlib/cmds/adm/shutdown.c
@@ -1,41 +1,61 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 1994-2026 Annihilator <taedlar@gmail.com>
 
-
+#include <ansi.h>
 #include <command.h>
 
 inherit F_CLEAN_UP;
 
-int
-main(object me, string arg)
-{
+int main (object me, string arg) {
     string wiz_status;
     object *user, link_ob;
     int i;
 
     SECURED_ADMIN_COMMAND;
 
-    seteuid(geteuid(this_player(1)));
+    mapping opts = ([
+        "prompt": "你確定要執行遊戲關閉程序嗎 (y/n)? ",
+        "options": ({ "No", "Yes" }),
+        "cursor": 0
+    ]);
+    get_char ("confirm_shutdown", 1, opts);
 
-    message("system", "遊戲重新啟動﹐請稍候一分鐘再 login 。\n", users() );
-
-    user = users();
-    for(i=0; i<sizeof(user); i++) {
-        if( !environment(user[i]) ) continue;
-        user[i]->save();
-    }
-
-    shutdown();
     return 1;
 }
-int help (object me)
-{
-        write(@HELP
+
+void confirm_shutdown (string arg, mapping opts) {
+    string answer = cursor_translate (arg, opts);
+    switch (answer ? answer : arg) {
+    case "Yes": case "Y": case "y":
+        seteuid (geteuid(this_player(1)));
+        message ("system", "遊戲關閉程序執行中﹐請稍候幾分鐘再嘗試連線。\n", users() );
+
+        foreach (object user in users()) {
+            if (!user.environment())
+                continue;
+            user->save();
+        }
+
+        write ("\r" CLR "\n"); // clear the line before shutdown
+        shutdown();
+        break;
+    case "No": case "N": case "n":
+        write ("\r" CLR "\n"); // clear the line
+        break;
+    default:
+        get_char ("confirm_shutdown", 1, opts);
+        break;
+    }
+}
+
+int help (object me) {
+    write (@HELP
 指令格式: shutdown
  
-馬上重新起動遊戲。
- 
+遊戲關閉程序。
 HELP
 );
-        return 1;
+    return 1;
 }
  
 
--- mudlib/feature/user/input.c
+++ mudlib/feature/user/input.c
@@ -1,7 +1,5 @@
-/*---
-description: Input prompt for get_char() and input_to() carryover mapping.
-author: Annihilator <taedlar@gmail.com>
----*/
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2026 Annihilator <taedlar@gmail.com>
 
 #include <ansi.h>
 
@@ -33,22 +31,22 @@ void input_prompt (mixed func, int flags, mixed data, mixed arg1) {
     if (mapp(data) && arrayp(data["options"]) && intp(data["cursor"])) {
         string prompt = data["prompt"] || "你的選擇：";
         int pos = 0;
-        write("\r" CLR "\n"); // placeholder for prompt
+        string ansi_menu = "\r" CLR "\n"; // placeholder for prompt
         foreach (string opt in data["options"]) {
             if (pos == data["cursor"])
-                write ("-> " BLK BWHT);
+                ansi_menu += "-> " BLK BWHT;
             else
-                write ("   ");
-            write (" " + opt + " ");
+                ansi_menu += "   ";
+            ansi_menu += " " + opt + " ";
             if (pos == data["cursor"]) {
-                write (NOR);
+                ansi_menu += NOR;
                 if (functionp (data["option_hints"]))
                     evaluate (data["option_hints"], opt);
             }
-            write ("\n");
+            ansi_menu += "\n";
             pos++;
         }
-        write (CUU (data["options"].len() + 1) + "\r" + prompt);
+        message ("prompt", ansi_menu + CUU (data["options"].len() + 1) + "\r" + prompt, this_object());
     }
     else if (func == "more" && arrayp (data) && intp (arg1)) {
         int page_size = this_object()->query("pager_size") || 22;
