04e9d89071c0ef0f1d0a637ab2984fc87561d9d3
Ted Chang <taedlar@gmail.com>
2026-09-22 16:19:54 +0800

    feat: add shutdown confirmation (#52)

--- mudlib/feature/char/message.c
+++ mudlib/feature/char/message.c
@@ -1,4 +1,5 @@
-
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 1994-2026 Annihilator <taedlar@gmail.com>
 
 #include <ansi.h>
 #include <dbase.h>
@@ -7,49 +8,55 @@
 
 static string *msg_buffer = ({});
 
-void
-receive_message(string msgclass, string msg)
-{
+void receive_message(string msgclass, string msg) {
     object link;
-    string subclass, *ch;
+    string category, *ch;
 
-    if( ! msg ) return;
+    if (! msg)
+        return;
 
-    if( ! interactive(this_object()) ) {
+    if (! interactive(this_object())) {
         this_object()->relay_message(msgclass, msg);
         return;
     }
 
-    sscanf(msgclass, "%s:%s", subclass, msgclass);
-
-    switch(subclass)
-    {
-    case "channel":
-	if( !objectp(link = this_object()->link())
-	||  !pointerp(ch = link->query("channels"))
-	||  member_array(msgclass, ch)==-1 )
-	    return;
-	break;
-    case "outdoor":
-	if( !environment() || !environment()->query("outdoors") )
-	    return;
-	break;
-    default:
-	if( this_player()
-	&&  (this_player() != this_object()) 
-	&&  !this_player()->visible(this_object()) )
-	    return;
+    if (msgclass == "prompt") {
+        receive (msg);
+        return;
     }
 
-    if( query_temp("block_msg/all")
-    ||	query_temp("block_msg/" + msgclass) )
-	return;
+    sscanf (msgclass, "%s:%s", category, msgclass);
 
-    if( in_input(this_object()) || in_edit(this_object()) ) {
-        if( sizeof(msg_buffer) < MAX_MSG_BUFFER )
+    switch (category)
+    {
+    case "channel":
+        if (!objectp(link = this_object()->link())
+        ||  !pointerp(ch = link->query("channels"))
+        ||  member_array(msgclass, ch)==-1 )
+            return;
+        break;
+    case "outdoor":
+        if (!environment() || !environment()->query("outdoors"))
+            return;
+        break;
+    default:
+        if (this_player()
+        &&  (this_player() != this_object()) 
+        &&  !this_player()->visible(this_object()) )
+            return;
+    }
+
+    if (query_temp("block_msg/all")
+    ||	query_temp("block_msg/" + msgclass) )
+        return;
+
+    if (in_input (this_object()) || in_edit (this_object()) ) {
+        if (sizeof(msg_buffer) < MAX_MSG_BUFFER)
             msg_buffer += ({ msg });
-    } else
-	receive( msg );
+        return;
+    }
+
+    receive (msg);
 }
 
 private void
@@ -57,10 +64,10 @@ write_prompt()
 {
     if( sizeof(msg_buffer) )
     {
-	receive(BOLD "[輸入時暫存訊息]\n" NOR);
-	foreach(string msg in msg_buffer)
-	    receive(msg);
-	msg_buffer = ({});
+        receive(BOLD "[輸入時暫存訊息]\n" NOR);
+        foreach(string msg in msg_buffer)
+            receive(msg);
+        msg_buffer = ({});
     }
     receive("> ");
 }
